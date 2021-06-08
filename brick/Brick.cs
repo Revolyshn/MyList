1120a74aae8bfb0a3798c85d5440addc08f91e40
Desepshn <84125241+Revolyshn@users.noreply.github.com>
2021-06-08 10:03:23 +0500

    created my own list

---
+++ brick/Brick.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brick
+{
+	class Brick<T>
+	{
+		public T value;
+
+		public Brick<T> next;
+
+		public Brick<T> prev;
+
+		public static int Count;
+
+		public Brick(T value)
+		{
+			this.value = value;
+		}		
+	}
+
+	class MyList<T>
+	{
+		public Brick<T> prev;
+		public Brick<T> head;
+		public Brick<T> tail;
+		public Brick<T> list;
+
+		public void Add(Brick<T> newBrick)
+		{
+			Brick<T>.Count++;
+			if (list != null)
+			{
+				GetLast().next = newBrick;
+				GetLast().prev = prev;
+				tail = GetLast();
+				prev = GetLast();
+				return;
+			}
+			
+			list = newBrick;
+			head = list;
+			prev = list;
+			tail = list;
+		}
+
+		public void PushFront(Brick<T> NewBrick)
+		{
+			Brick<T> brick = NewBrick;
+			brick.next = head;
+			head.prev = brick;
+			head = brick;
+		}
+
+		public Brick<T> GetLast()
+		{
+			Brick<T> current = list;
+			while(current.next != null)
+			{
+				current = current.next;
+			}
+			return current;
+
+		}
+
+		public void PrintBack()
+		{
+			Brick<T> Tail = tail;
+			while (Tail != null)
+			{
+				Console.WriteLine(Tail.value);
+				Tail = Tail.prev;
+			}
+			Console.WriteLine();
+		}
+
+		public void Print()
+		{
+			Brick<T> Head = head;
+			while (Head != null)
+			{
+				Console.WriteLine(Head.value);
+				Head = Head.next;
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ brick/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brick
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<int> lst = new MyList<int>();
+			lst.Add(new Brick<int>(1));
+			lst.Add(new Brick<int>(2));
+			lst.Add(new Brick<int>(2));
+			lst.Add(new Brick<int>(3));
+			lst.Add(new Brick<int>(4));
+			lst.Add(new Brick<int>(5));
+			lst.PushFront(new Brick<int>(100));
+			
+			lst.Print();
+			lst.PrintBack();
+			Console.ReadLine();
+		}
+	}
+}
